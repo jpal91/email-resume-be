a25ab6f72f4db23dd1d41166cae4207abe8fa222
jpal91 <pallanschja@gmail.com>
2023-02-06 09:23:28 -0500

    updated initial configuration

--- cloudformation/create.sh
+++ cloudformation/create.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 aws cloudformation create-stack --stack-name ServerlessEmail --template-body file:///home/jpal/dev/email-resume-be/config.yaml \
---parameters ParameterKey=EmailIdentityName,ParameterValue="myserverlessemail.info" ParameterKey=NameOfRecipient,ParameterValue="justin@myserverlessemail.info" \
+--parameters ParameterKey=NameOfRecipient,ParameterValue="justin@myserverlessemail.info" \
 ParameterKey=S3BucketName,ParameterValue="serverless-email-8080" ParameterKey=HostedZoneName,ParameterValue="myserverlessemail.info" \
 ParameterKey=ZoneID,ParameterValue="Z09510269JSPHJGKHXSG" --capabilities CAPABILITY_NAMED_IAM
